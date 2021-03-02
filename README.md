584817ed71db5806e2fc7fbba75d314108394ea7
daniel-kanchev <daniel.kanchev@adata.pro>
2021-03-02 11:55:08 +0200

    Finished Script

---
+++ README.md
@@ -0,0 +1,11 @@
+URL: https://www.carnegie.se/om-carnegie/pressrum/pressmeddelanden/
+
+    Spider name: carnegie
+
+DB Schema:
+- title
+- date
+- link
+- content
+
+Notes:
---
+++ carnegie/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'carnegie'
+SPIDER_MODULES = ['carnegie.spiders']
+NEWSPIDER_MODULE = 'carnegie.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'carnegie.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
